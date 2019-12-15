da9b3a58d28f9e37f216eb36f137dff9e3178436
Alexander Kalinin <alex.kalinin@tray.io>
2019-12-15 20:19:45 +0000

    chage to gatsby

---
+++ gatsby-starter-hero-blog/content/meta/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: "Alex Kalinin - Blog", // <title>
+  shortSiteTitle: "Blog", // <title> ending for posts and pages
+  siteDescription: "Alex Kalinin Blog",
+  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "Alex Kalinin",
+  // authorTwitterAccount: "greglobinski",
+
+  /* info */
+  headerTitle: "Alex Kalinin",
+  headerSubTitle: "presents another one GatsbyJS starter",
+
+  /* manifest.json */
+  manifestName: "HeroBlog - a GatsbyJS starter",
+  manifestShortName: "HeroBlog", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/kalininator" }
+    // { name: "twitter", url: "https://twitter.com/greglobinski" },
+    // { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
